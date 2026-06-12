0ae515e175e91a5ab8d5df719eae0b3508e64dfa
aymane2799 <aymane.ouladbenhamou@gmail.com>
2026-06-13 00:45:43 +0100

    fix: application launch

--- src/main/java/com/rently/rently/config/MultiTenancyConfig.java
+++ src/main/java/com/rently/rently/config/MultiTenancyConfig.java
@@ -1,22 +1,68 @@
 package com.rently.rently.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
-import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MultiTenancyConfig {
 
-    @Bean
-    public HibernatePropertiesCustomizer multiTenancyCustomizer(
+    @Value("${spring.jpa.hibernate.ddl-auto:update}")
+    private String ddlAuto;
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource,
             MultiTenantConnectionProvider<String> connectionProvider,
-            CurrentTenantIdentifierResolver<String> tenantResolver) {
-        return properties -> {
-            properties.put(AvailableSettings.MULTI_TENANT_CONNECTION_PROVIDER, connectionProvider);
-            properties.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, tenantResolver);
-        };
+            CurrentTenantIdentifierResolver<String> tenantResolver,
+            Environment environment) {
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factory.setPackagesToScan("com.rently.rently");
+        factory.setPersistenceUnitName("default");
+
+        // Bind all spring.jpa.properties.* from application.yaml
+        Map<String, Object> props = new HashMap<>(
+                Binder.get(environment)
+                      .bind("spring.jpa.properties", Bindable.mapOf(String.class, String.class))
+                      .orElseGet(HashMap::new)
+        );
+
+        // Apply spring.jpa.hibernate.ddl-auto
+        props.put("hibernate.hbm2ddl.auto", ddlAuto);
+
+        // Apply multi-tenancy infrastructure (overrides any yaml value)
+        props.put(AvailableSettings.MULTI_TENANT_CONNECTION_PROVIDER, connectionProvider);
+        props.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, tenantResolver);
+
+        factory.setJpaPropertyMap(props);
+        return factory;
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 }
